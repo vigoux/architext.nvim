3f58686b625415489e29c6c23f3202b71e131b37
Thomas Vigouroux <thomas.vigouroux@univ-grenoble-alpes.fr>
2023-05-10 08:44:42 +0200

    fix: use newer ts api

--- lua/architext/transform.lua
+++ lua/architext/transform.lua
@@ -1,17 +1,17 @@
-local ts_q = require'vim.treesitter.query'
+local ts = vim.treesitter
 
 local M = {}
 
 local transforms = {
   ["upper"] = function(bufnr, node)
-    local node_text = ts_q.get_node_text(node, bufnr)
+    local node_text = ts.get_node_text(node, bufnr)
     if not node_text then return end
 
     return vim.fn.toupper(node_text)
   end,
 
   ["lower"] = function(bufnr, node)
-    local node_text = ts_q.get_node_text(node, bufnr)
+    local node_text = ts.get_node_text(node, bufnr)
     if not node_text then return end
 
     return vim.fn.tolower(node_text)
@@ -21,8 +21,8 @@ local transforms = {
 function M.apply(transform_name, buf, node)
   local transform = transforms[transform_name or "__EMPTY__"]
 
-  if not transform then return ts_q.get_node_text(node, buf) end
-  return transform(buf, node) or ts_q.get_node_text(node, buf)
+  if not transform then return ts.get_node_text(node, buf) end
+  return transform(buf, node) or ts.get_node_text(node, buf)
 end
 
 function M.add(name, handler)
